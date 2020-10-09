9e195d60187352e52e21185b5d786fb41c1a2a81
sarkisova_ea <sarkisova_ea@sptnk.co>
2020-10-09 17:11:07 +0400

    add images, font

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.sass';
+
+function App() {
+  return (
+    <div className="app">
+      <header className="header">
+        <h1>Ты сегодня покормил кота?</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
