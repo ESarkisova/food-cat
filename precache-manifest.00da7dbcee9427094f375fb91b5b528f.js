2daea07a4d36fa6706cad84985f63c25ba942665
sarkisova_ea <sarkisova_ea@sptnk.co>
2020-10-10 14:21:53 +0400

    Updates

---
+++ precache-manifest.00da7dbcee9427094f375fb91b5b528f.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a043d5b8a8bfd2afb2d0f84e87fe7b2",
+    "url": "/food-cat/index.html"
+  },
+  {
+    "revision": "1db2b3d5f1d6429c236b",
+    "url": "/food-cat/static/css/main.3402ce1b.chunk.css"
+  },
+  {
+    "revision": "265e6652880d1ca82e31",
+    "url": "/food-cat/static/js/2.dd294165.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/food-cat/static/js/2.dd294165.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1db2b3d5f1d6429c236b",
+    "url": "/food-cat/static/js/main.409c54c0.chunk.js"
+  },
+  {
+    "revision": "b9bf519beb49bf5702b7",
+    "url": "/food-cat/static/js/runtime-main.d4f2edbe.js"
+  },
+  {
+    "revision": "5403f35ca4bad6e702d74c7c9dbb49ca",
+    "url": "/food-cat/static/media/Exo2-Bold.5403f35c.eot"
+  },
+  {
+    "revision": "b66e302a42655bd67e3059548dec991f",
+    "url": "/food-cat/static/media/Exo2-Bold.b66e302a.ttf"
+  },
+  {
+    "revision": "bc52252d4e468215d2b76ff879de9543",
+    "url": "/food-cat/static/media/Exo2-Bold.bc52252d.woff"
+  },
+  {
+    "revision": "0e6e579ba60276b68c314f38fc266e3f",
+    "url": "/food-cat/static/media/Exo2.0-Thin.0e6e579b.eot"
+  },
+  {
+    "revision": "c33b184f5dd1056361d472fd0a43e6a5",
+    "url": "/food-cat/static/media/Exo2.0-Thin.c33b184f.woff"
+  },
+  {
+    "revision": "cdad79eba3ba1db2da9380409a9fde10",
+    "url": "/food-cat/static/media/Exo2.0-Thin.cdad79eb.ttf"
+  },
+  {
+    "revision": "4ca5cb60bad508438f6f4674c0eddf64",
+    "url": "/food-cat/static/media/TrebuchetMS-Bold.4ca5cb60.eot"
+  },
+  {
+    "revision": "6cbec14f7ae96def20259b23fb722619",
+    "url": "/food-cat/static/media/TrebuchetMS-Bold.6cbec14f.ttf"
+  },
+  {
+    "revision": "e2683e486126f7d30684f98569e053b4",
+    "url": "/food-cat/static/media/TrebuchetMS-Bold.e2683e48.woff"
+  },
+  {
+    "revision": "0db46deef103704c3760ea5367ba74a1",
+    "url": "/food-cat/static/media/TrebuchetMS.0db46dee.woff"
+  },
+  {
+    "revision": "48c4ef39806ef76eaeced814b8a5705c",
+    "url": "/food-cat/static/media/TrebuchetMS.48c4ef39.ttf"
+  },
+  {
+    "revision": "a8b28478e198c0b309c6ac9d09624fbd",
+    "url": "/food-cat/static/media/TrebuchetMS.a8b28478.eot"
+  },
+  {
+    "revision": "d2e53f3ede20e531b9a861d8b27f5c09",
+    "url": "/food-cat/static/media/bg-main.d2e53f3e.jpg"
+  }
+]);
